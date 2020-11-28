e92baed971f4223aa4605aa9847cf1b0cb0107e6
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 02:59:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,5 +26,5 @@ def get_english_meaning(file_path, emoticon)
   emoticon = library.keys.find do |key|
     library[key][:japanese] == emoticon
   end
-  emoticon ? emoticon : "Sorry, that emoticon doesn't exist!"
+  emoticon ? emoticon : "Sorry that emoticon doesn't exist!"
 end
