9785355c6826ed3939db52fdbec0b29f7f842e16
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 02:54:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_file(file_path)
+  library = load_file(file_path)
   emoticons = library.keys.find do |key|
     library[key][:english] == emoticon
   end
