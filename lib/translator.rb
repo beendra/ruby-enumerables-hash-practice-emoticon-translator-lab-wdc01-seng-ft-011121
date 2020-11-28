cdfb4948fe69fc775533c27725543f821e2752dc
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 02:35:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require 'pry'
 require "yaml"
 
-def load_library(file_path)
-  emoticons = YAML.load_file(file_path)
+def load_library
+  emoticons = YAML.load_file("/lib/emoticons.yml")
 end
 
 def get_japanese_emoticon
